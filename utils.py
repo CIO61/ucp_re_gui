c90a0bd0a26b0ff06b3c8b9f4077e730286d2253
CIO61 <enst61@gmail.com>
2021-08-30 13:50:44 +0300

    refactoring/getting rid of unnecessary imports, albeit redundant but lazy-to-clear UI updates

--- utils.py
+++ utils.py
@@ -1,15 +1,15 @@
-import regex
+import re
 
 
 def preprocess_json(file_str):
-    pattern = regex.compile(': ?"([^"]*?\n)[^"]*?"')
+    pattern = re.compile(': ?"([^"]*?\n)[^"]*?"')
     pos = 0
-    while regex.search(pattern, file_str[pos:]) is not None:
-        span = regex.search(pattern, file_str[pos:]).span()
+    while re.search(pattern, file_str[pos:]) is not None:
+        span = re.search(pattern, file_str[pos:]).span()
         file_str = file_str.replace(file_str[pos + span[0]:pos + span[1]],
                                     file_str[pos + span[0]:pos + span[1]].replace("\t", "\\t"))
 
-        span = regex.search(pattern, file_str[pos:]).span()
+        span = re.search(pattern, file_str[pos:]).span()
         pos_ = span[0] + len(file_str[pos + span[0]:pos + span[1]].replace("\n", "\\n"))
         file_str = file_str.replace(file_str[pos + span[0]:pos + span[1]],
                                     file_str[pos + span[0]:pos + span[1]].replace("\n", "\\n"))
